4a07f52f4efe4e6f5222c7217db9648261e06ab6
함명연 <hmy751@gmail.com>
2025-05-30 01:37:06 +0900

    feat: Input 컴포넌트 및 스토리북 구현

---
+++ src/component/Input/index.stories.tsx
@@ -0,0 +1,160 @@
+import type { Meta, StoryObj } from "@storybook/react-webpack5";
+import Input from "./index";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import Button from "../Button";
+
+const meta = {
+  title: "Common/Input",
+  component: Input,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {
+    placeholder: {
+      control: {
+        type: "text",
+      },
+    },
+  },
+} satisfies Meta<typeof Input>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    placeholder: "Enter Text...",
+  },
+  render() {
+    const [isFocused, setIsFocused] = useState(false);
+    const [isTouched, setIsTouched] = useState(false);
+
+    return (
+      <div>
+        <Input
+          {...Primary.args}
+          isFocused={isFocused}
+          isTouched={isTouched}
+          onFocusChange={setIsFocused}
+          onTouchChange={setIsTouched}
+        />
+      </div>
+    );
+  },
+};
+
+export const Focused: Story = {
+  args: {
+    placeholder: "Enter Text...",
+  },
+  render() {
+    const [isTouched, setIsTouched] = useState(true);
+    const [isFocused, setIsFocused] = useState(true);
+    return (
+      <div>
+        <Input
+          placeholder="Enter Text..."
+          isFocused={isFocused}
+          isTouched={isTouched}
+          onFocusChange={setIsFocused}
+          onTouchChange={setIsTouched}
+        />
+      </div>
+    );
+  },
+};
+
+export const Touched: Story = {
+  args: {
+    placeholder: "Enter Text...",
+  },
+  render() {
+    const [isTouched, setIsTouched] = useState(true);
+    const [isFocused, setIsFocused] = useState(false);
+
+    return (
+      <div>
+        <Input
+          placeholder="Enter Text..."
+          isTouched={isTouched}
+          isFocused={isFocused}
+          onTouchChange={setIsTouched}
+          onFocusChange={setIsFocused}
+        />
+      </div>
+    );
+  },
+};
+
+export const Error: Story = {
+  args: {
+    placeholder: "Enter Text...",
+  },
+  render() {
+    const [isFocused, setIsFocused] = useState(false);
+    const [isTouched, setIsTouched] = useState(false);
+    const [errorMessage, setErrorMessage] = useState("Error");
+    return (
+      <div>
+        <Input
+          placeholder="Enter Text..."
+          isFocused={isFocused}
+          isTouched={isTouched}
+          onFocusChange={setIsFocused}
+          onTouchChange={setIsTouched}
+          isError={errorMessage}
+        />
+      </div>
+    );
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    placeholder: "Enter Text...",
+  },
+  render() {
+    const [isTouched, setIsTouched] = useState(false);
+    const [isFocused, setIsFocused] = useState(false);
+    return (
+      <div>
+        <Input
+          placeholder="Enter Text..."
+          isDisabled={true}
+          isTouched={isTouched}
+          isFocused={isFocused}
+        />
+      </div>
+    );
+  },
+};
+
+// 내 컴포넌트는 내부에 touch처리를해서 지금 잘 되지만, 일반적으로 touche를 적용하려면 Controller를 사용해야 한다.
+export const UseHookForm = {
+  render: () => {
+    const { control, register } = useForm({
+      defaultValues: {
+        name: "",
+      },
+    });
+
+    const handleClick = () => {
+      console.log(control._formValues);
+    };
+
+    return (
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignContent: "center",
+        }}
+      >
+        <Input {...register("name")} />
+        <Button onClick={handleClick}>출력하기</Button>
+      </div>
+    );
+  },
+};
---
+++ src/component/Input/index.tsx
@@ -0,0 +1,146 @@
+import {
+  forwardRef,
+  useEffect,
+  useRef,
+  useCallback,
+  InputHTMLAttributes,
+  FocusEvent,
+  RefObject,
+  useState,
+  TouchEvent,
+  MouseEvent,
+} from "react";
+import clsx from "clsx";
+import styles from "./index.module.css";
+
+export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  isFocused?: boolean;
+  isTouched?: boolean;
+  isError?: string;
+  placeholder?: string;
+  isDisabled?: boolean;
+  onFocusChange?: (isFocused: boolean) => void;
+  onTouchChange?: (isTouched: boolean) => void;
+}
+
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  (
+    {
+      isFocused: controlledFocus,
+      onFocus,
+      onFocusChange,
+      onBlur,
+      isTouched: controlledTouch,
+      onTouchStart,
+      onTouchChange,
+      onMouseDown,
+      isError,
+      placeholder,
+      isDisabled,
+      className,
+      disabled,
+      ...restProps
+    }: InputProps,
+    ref: React.Ref<HTMLInputElement>
+  ): React.ReactElement => {
+    // ref 처리
+    const internalInputRef = useRef<HTMLInputElement>(null);
+
+    const assignRef = useCallback(
+      (element: HTMLInputElement) => {
+        if (element) {
+          (internalInputRef as RefObject<HTMLInputElement>).current = element;
+        }
+
+        if (typeof ref === "function") {
+          ref(element);
+        } else if (ref) {
+          (ref as RefObject<HTMLInputElement>).current = element;
+        }
+      },
+      [ref]
+    );
+
+    // isTouched와 같은 value는 control, unControl에 따라서 기준을 정하고,
+    // 이벤트는 control, unControl 모두 실행하여 일관성을 유지한다.
+
+    // touch 이벤트
+    const [unControlledTouch, setUnControlledTouch] = useState<boolean>(false);
+    const isControlledTouch = controlledTouch !== undefined;
+
+    const isTouched = isControlledTouch ? controlledTouch : unControlledTouch;
+
+    const handleTouch = (
+      e: TouchEvent<HTMLInputElement> | MouseEvent<HTMLInputElement>
+    ) => {
+      e.stopPropagation();
+
+      // 단순 상위에서 조정하는 이벤트
+      onTouchChange?.(true);
+
+      // 내부 이벤트
+      setUnControlledTouch(true);
+
+      // 상위 useForm과 같은 controlled 이벤트
+      onTouchStart?.(e as TouchEvent<HTMLInputElement>);
+      onMouseDown?.(e as MouseEvent<HTMLInputElement>);
+    };
+
+    // focus 이벤트
+    const [unControlledFocus, setUncontrolledFocus] = useState<boolean>(false);
+    const isControlledFocus = controlledFocus !== undefined;
+
+    const isFocused = isControlledFocus ? controlledFocus : unControlledFocus;
+
+    const handleFocus = useCallback(
+      (e: FocusEvent<HTMLInputElement>) => {
+        e.stopPropagation();
+
+        onFocusChange?.(true);
+
+        setUncontrolledFocus(true);
+        onFocus?.(e);
+      },
+      [onFocusChange, onFocus]
+    );
+
+    // blur 이벤트
+    const handleBlur = useCallback(
+      (e: FocusEvent<HTMLInputElement>) => {
+        e.stopPropagation();
+
+        onFocusChange?.(false);
+
+        setUncontrolledFocus(false);
+
+        onBlur?.(e);
+      },
+      [onFocusChange, onBlur, isControlledFocus]
+    );
+
+    return (
+      <input
+        ref={assignRef}
+        placeholder={placeholder}
+        disabled={disabled}
+        className={clsx(
+          styles.input,
+          isTouched && styles.isTouched,
+          isFocused && styles.isFocused,
+          isError && styles.isError,
+          disabled && styles.isDisabled,
+          className
+        )}
+        onMouseDown={handleTouch}
+        onTouchStart={handleTouch}
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+        {...restProps}
+      />
+    );
+  }
+);
+
+Input.displayName = "Input";
+
+export default Input;
