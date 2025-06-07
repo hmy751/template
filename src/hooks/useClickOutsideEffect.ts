4ce07043ea10693af3625f8e6a72e7586d5f37dd
함명연 <hmy751@gmail.com>
2025-06-07 18:06:06 +0900

    feat: useClickOutsideEffect hook 구현

---
+++ src/hooks/useClickOutsideEffect.ts
@@ -0,0 +1,26 @@
+import { useEffect } from "react";
+
+const useClickOutsideEffect = (
+  ref: React.RefObject<HTMLElement | null>,
+  handler: () => void
+) => {
+  useEffect(() => {
+    const listener = (e: MouseEvent | TouchEvent) => {
+      if (!ref.current || ref.current.contains(e.target as Node)) {
+        return;
+      }
+
+      handler();
+    };
+
+    document.addEventListener("mousedown", listener);
+    document.addEventListener("touchstart", listener);
+
+    return () => {
+      document.removeEventListener("mousedown", listener);
+      document.removeEventListener("touchstart", listener);
+    };
+  }, [ref, handler]);
+};
+
+export default useClickOutsideEffect;
